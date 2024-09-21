032b27e579bd5a8c6b0db8cc457a6feea013b1e7
22112-code <ncfamily4barishal@gmail.com>
2024-09-21 23:52:52 +0600

    Add files via upload

--- test get.java
+++ test get.java
@@ -4,8 +4,8 @@ class Person {
 
     //Make a default constructor
     public Person() {
-        System.out.println("Naba Chowdhury");
-        System.out.println("20");
+         System.out.println("Naba Chowdhury");
+       System.out.println("age 20");
         //Write your code here...
 
     }
@@ -24,7 +24,7 @@ class Person {
 
     public int getAge() {
         return age;
-    }
+    
 }
 
 class ICE {
@@ -34,9 +34,10 @@ class ICE {
         Person person2= new Person("Nuhat Chowdhury",12);
         //Create an object named "person1" by Default constructor.
         //Create an object named "person2" by Parameterized constructor.
-person1.Person();
+
         // Accessing object attributes
-        System.out.println("Person 1 - Name: " + person1.getName() + ", Age: " + person1.getAge());
+        System.out.println("Person 1 - Name: " + person1.Person() + ", Age: " + person1.Person());
         System.out.println("Person 2 - Name: " + person2.getName() + ", Age: " + person2.getAge());
+        
     }
-}
+}}
